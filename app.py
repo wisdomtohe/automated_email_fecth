69c129e6804086b31e636f537054fe4476d7c07b
Wisdom TOHE <38359803+wisdomtohe@users.noreply.github.com>
2021-05-25 11:14:13 +0000

    script added for email feching

---
+++ app.py
@@ -0,0 +1,46 @@
+import imaplib
+import base64
+import os
+import email
+
+email_user = input('Email: ')
+email_pass = input('Password: DWspLNO@b5 ')
+
+mail = imaplib.IMAP4_SSL('imap.psdttgroup.com',993)
+mail.login(email_user, email_pass)
+mail.select("INBOX")
+
+type, data = mail.search(None, 'ALL')
+mail_ids = data[0]
+id_list = mail_ids.split()
+
+for num in data[0].split():
+    typ, data = mail.fetch(num, '(RFC822)' )
+    raw_email = data[0][1]# converts byte literal to string removing b''
+    raw_email_string = raw_email.decode('utf-8')
+    email_message = email.message_from_string(raw_email_string)# downloading attachments
+    for part in email_message.walk():
+        # this part comes from the snipped I don't understand yet... 
+        if part.get_content_maintype() == 'multipart':
+            continue
+        if part.get('Content-Disposition') is None:
+            continue
+        fileName = part.get_filename()
+        if bool(fileName):
+            filePath = os.path.join('/Users/psdtt/Documents/Data_Analysis/Prog/', fileName)
+            if not os.path.isfile(filePath) :
+                fp = open(filePath, 'wb')
+                fp.write(part.get_payload(decode=True))
+                fp.close()
+            subject = str(email_message).split("Subject: ", 1)[1].split("\nTo:", 1)[0]
+            # print('Downloaded "{file}" from email titled "{subject}" with UID {uid}.'.format(file=fileName, subject=subject, uid=latest_email_uid.decode('utf-8')))
+
+
+    for response_part in data:
+            if isinstance(response_part, tuple):
+                msg = email.message_from_string(response_part[1].decode('utf-8'))
+                email_subject = msg['subject']
+                email_from = msg['from']
+                print ('From : ' + email_from + '\n')
+                print ('Subject : ' + email_subject + '\n')
+                print(msg.get_payload(decode=True))
